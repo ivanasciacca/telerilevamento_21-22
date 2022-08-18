bcbe2bce1916a2618232a3649d68eb9431798a71
ivanasciacca <80622661+ivanasciacca@users.noreply.github.com>
2022-08-18 19:57:20 +0200

    Create R_code_LiDAR.r

---
+++ R_code_LiDAR.r
@@ -0,0 +1,10 @@
+# R code for visualising and analysing LiDAR data
+
+# Caricare le librerie 
+library(raster)
+library(rgdal)
+library(viridis)
+library(ggplot2)
+
+# Settare la cartella di lavoro
+setwd("C:/lab/")
