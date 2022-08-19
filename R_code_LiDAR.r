5f00da44ad5eb6201c4708759e0784cf9e67aa68
ivanasciacca <80622661+ivanasciacca@users.noreply.github.com>
2022-08-19 11:57:28 +0200

    Update R_code_LiDAR.r

--- R_code_LiDAR.r
+++ R_code_LiDAR.r
@@ -8,3 +8,25 @@ library(ggplot2)
 
 # Settare la cartella di lavoro
 setwd("C:/lab/")
+
+#
+dsm_2013 <- raster("2013Elevation_DigitalElevationModel-0.5m.tif")
+dsm_2013
+
+#
+dtm_2013 <- raster("2013Elevation_DigitalTerrainModel-0.5m.tif")
+dtm_2013
+
+#
+plot(dtm_2013)
+
+#
+chm_2013 <- dsm_2013 - dtm_2013
+chm_2013
+
+#
+ggplot() + 
+  geom_raster(chm_2013, mapping =aes(x=x, y=y, fill=layer)) + 
+  scale_fill_viridis() +
+  ggtitle("CHM 2013 San Genesio/Jenesien")
+
