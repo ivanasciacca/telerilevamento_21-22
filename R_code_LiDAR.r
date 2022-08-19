5bc3662d576694053f8e764c9496dc4c5c428647
ivanasciacca <80622661+ivanasciacca@users.noreply.github.com>
2022-08-19 12:56:40 +0200

    Update R_code_LiDAR.r

--- R_code_LiDAR.r
+++ R_code_LiDAR.r
@@ -30,3 +30,47 @@ ggplot() +
   scale_fill_viridis() +
   ggtitle("CHM 2013 San Genesio/Jenesien")
 
+
+# 2004
+dsm_2004 <- raster("2004Elevation_DigitalElevationModel-2.5m.tif")
+dsm_2004
+
+#
+dtm_2004 <- raster("2004Elevation_DigitalTerrainModel-2.5m.tif")
+dtm_2004
+
+#
+plot(dtm_2004)
+
+#
+chm_2004 <- dsm_2004 - dtm_2004
+chm_2004
+
+#
+ggplot() + 
+  geom_raster(chm_2004, mapping =aes(x=x, y=y, fill=layer)) + 
+  scale_fill_viridis() +
+  ggtitle("CHM 2004 San Genesio/Jenesien")
+
+
+# Calcolare la differenza
+difference_chm <- chm_20013-chm_2004
+
+# Resampling
+chm_2013r <- resample(chm_2013, chm_2004)
+difference_chm <- chm_2013r-chm_2004
+
+ggplot() +
+geom_raster(difference_chm, mapping =aes(x=x, y=y, fill=layer)) + 
+  scale_fill_viridis() +
+  ggtitle("CHM difference San Genesio/Jenesien")
+
+# Reading the point cloud
+point_cloud <- readLAS("point_cloud.laz")
+
+plot(point_cloud)
+                    
+
+
+
+
