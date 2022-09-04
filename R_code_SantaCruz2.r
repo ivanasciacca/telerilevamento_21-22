eeb91160ae807ee21d24632323e70da80a525c47
ivanasciacca <80622661+ivanasciacca@users.noreply.github.com>
2022-09-04 14:35:55 +0200

    Update R_code_SantaCruz2.r

--- R_code_SantaCruz2.r
+++ R_code_SantaCruz2.r
@@ -7,7 +7,7 @@ setwd("C:/lab/Santa Cruz")
 
 
 
-  ##  2002  ##
+   ##  2002  ##
   
 # Caricare i RasterLayer delle singole bande del 2002
 # B1 = banda del blu
@@ -15,28 +15,74 @@ setwd("C:/lab/Santa Cruz")
 # B3 = banda del rosso
 # B4 = banda de NIR
 
-lst02_1 <- raster("LC09_L2SP_230072_20220814_20220816_02_T1_SR_B1.tif")
-lst02_1
+B1_2002 <- raster("LE07_L2SP_230072_20020620_20200916_02_T1_SR_B1.tif")
+B1_2002
 
-lst02_2 <- raster("LC09_L2SP_230072_20220814_20220816_02_T1_SR_B2.tif")
-lst02_2
+B2_2002 <- raster("LE07_L2SP_230072_20020620_20200916_02_T1_SR_B2.tif")
+B2_2002
 
-lst02_3 <- raster("LC09_L2SP_230072_20220814_20220816_02_T1_SR_B3.tif")
-lst02_3
+B3_2002 <- raster("LE07_L2SP_230072_20020620_20200916_02_T1_SR_B3.tif")
+B3_2002
 
-lst02_4 <- raster("LC09_L2SP_230072_20220814_20220816_02_T1_SR_B4.tif")
-lst02_4
+B4_2002 <- raster("LE07_L2SP_230072_20020620_20200916_02_T1_SR_B4.tif")
+B4_2002
 
-# Importare i dati tutti insieme, tramite la funzione list.fils che crea una lista di file
-# patter = ciò che hanno in comune questi file, in questo caso è lst
-
-rlist02 <- list.files(pattern="lst02")
-rlist02
-
-# Applicare la funzione raster all'intera lista, importare i 4 file
-import <- lapply(rlist02, raster)
-import
+# Importare i dati tutti insieme, tramite la funzione list che crea una lista di file
+rlist02 <- list(B1_2002, B2_2002, B3_2002, B4_2002)
 
 # La funzione stack ci consente di mettere insieme tutti i file in un blocco comune
-tgr <- stack(import)
-tgr
+# Con la funzione plot, visualizziamo l'immagine
+ln2002 <- stack(rlist02)
+ln2002
+plot(ln2002)
+
+# Con la funzione plotRGB è possibile visualizzare l'immagine a colori naturali, r = 3, g = 2, b = 1 
+plotRGB(ln2002, 3, 2, 1, stretch="hist")
+
+
+
+
+
+
+
+ ## 2022 ##
+
+# Caricare i RasterLayer delle singole bande del 2022
+# B1 = banda del blu
+# B2 = banda del verde
+# B3 = banda del rosso
+# B4 = banda de NIR
+
+B1_2022 <- raster("LC09_L2SP_230072_20220814_20220816_02_T1_SR_B1.tif")
+B1_2022
+
+B2_2022 <- raster("LC09_L2SP_230072_20220814_20220816_02_T1_SR_B2.tif")
+B2_2022
+
+B3_2022 <- raster("LC09_L2SP_230072_20220814_20220816_02_T1_SR_B3.tif")
+B3_2022
+
+B4_2022 <- raster("LC09_L2SP_230072_20220814_20220816_02_T1_SR_B4.tif")
+B4_2022
+
+
+# Importare i dati tutti insieme, tramite la funzione list che crea una lista di file
+rlist22 <- list(B1_2022, B2_2022, B3_2022, B4_2022)
+
+# La funzione stack ci consente di mettere insieme tutti i file in un blocco comune
+# Con la funzione plot, visualizziamo l'immagine
+ln2022 <- stack(rlist22)
+ln2022
+plot(ln2022)
+
+# Con la funzione plotRGB è possibile visualizzare l'immagine a colori naturali, r = 3, g = 2, b = 1 
+plotRGB(ln2022, 3, 2, 1, stretch="hist")
+
+
+
+
+
+
+
+
+
