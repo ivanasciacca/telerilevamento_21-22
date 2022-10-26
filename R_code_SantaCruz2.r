b28cd9fbd37c1a92a23aacb9228fe590a3762509
ivanasciacca <80622661+ivanasciacca@users.noreply.github.com>
2022-10-26 18:23:59 +0200

    Update R_code_SantaCruz2.r

--- R_code_SantaCruz2.r
+++ R_code_SantaCruz2.r
@@ -267,140 +267,95 @@ dev.off()
 
    ## Land cover ##
 
-# Classificare l'immagine del 2002 in due classi
+ # Classificare l'immagine del 2002 in due classi
 # Classe 1 = zona forestale (bianco)
 # Classe 2 = suolo agricolo (verde)
 
 ln2002nC <- unsuperClass(ln2002, nClasses=2)
 ln2002nC
-
-jpeg("ln2002nC")
 plot(ln2002nC$map)
-dev.off()
 
 # Calcolare la frequenza dei pixel che appartengono a ciascuna classe 
 freq(ln2002nC$map)
 
-# Classe 1 = 25037359
-# Classe 2 = 12948482
+# Classe 1 = 25409424
+# Classe 2 = 12576417
 # NA 17433870 (non applicabile)
 
 # Calcolare la percentuale di foresta e di area agricola del 2002
 tot2002 <- 55419711
 
-perc_forest_2002 <- 25037359 * 100 / tot2002
+perc_forest_2002 <- 25409424 * 100 / tot2002
 perc_forest_2002
-# Percentuale foresta = 45.17771
+# Percentuale foresta = 45.84907
 
-perc_agr_2002 <- 12948482 * 100 / tot2002
+perc_agr_2002 <- 12576417 * 100 / tot2002
 perc_agr_2002
-# Percentuale suolo agricolo = 23.3644
+# Percentuale suolo agricolo = 22.69304
 
 
 
-# Classificare l'immagine del 2014 in due classi
-# Classe 1 = zona forestale (bianco)
-# Classe 2 = suolo agricolo (verde)
+ # Classificare l'immagine del 2014 in due classi
+# Classe 1 = zona agricola (bianca)
+# Classe 2 = suolo forestale (verde)
 
 ln2014nC <- unsuperClass(ln2014, nClasses=2)
 ln2014nC
-
-jpeg("ln2014nC")
 plot(ln2014nC$map)
-dev.off()
 
 # Calcolare la frequenza dei pixel che appartengono a ciascuna classe 
 freq(ln2014nC$map)
 
-# Classe 1 = 23641752
+# Classe 1 = 23641752 
 # Classe 2 = 17040113
 # NA 18928376 (non applicabile)
 
 # Calcolare la percentuale di foresta e di area agricola del 2014
 tot2014 <- 59610241 
 
-perc_forest_2014 <- 23641752 * 100 / tot2014
+perc_forest_2014 <- 17040113 * 100 / tot2014
 perc_forest_2014
-# Percentuale foresta = 39.66055
+# Percentuale foresta = 28.58588
 
-perc_agr_2014 <- 17040113 * 100 / tot2014
+perc_agr_2014 <- 23641752 * 100 / tot2014
 perc_agr_2014
-# Percentuale zona agricola = 28.58588
+# Percentuale zona agricola = 39.66055
 
 
 
-# Classificare l'immagine del 2022 in due classi
-
-   ## Prova 1 ##
-
+ # Classificare l'immagine del 2022 in due classi
 # Classe 1 = zona forestale (bianco) 
 # Classe 2 = suolo agricolo (verde)
 
 ln22nC <- unsuperClass(ln2022, nClasses=2)
 ln22nC
-
-jpeg("ln22nC")
 plot(ln22nC$map)
-dev.off()
 
 # Calcolare la frequenza dei pixel che appartengono a ciascuna classe 
 freq(ln22nC$map)
 
-# Classe 1 = 40480835
-# Classe 2 = 313073
+# Classe 1 = 314766
+# Classe 2 = 40480835
 # NA 18663113 (non applicabile)
 
 # Calcolare la percentuale di foresta e di area agricola del 2022
 tot22 <- 59457021
 
-perc_forest_22 <- 40480835 * 100 / tot22
+perc_forest_22 <- 314766 * 100 / tot22
 perc_forest_22
-# Percentuale foresta = 68.0842
+# Percentuale foresta = 0.5294009
 
-perc_agr_22 <- 313073 * 100 / tot22
+perc_agr_22 <- 40480835 * 100 / tot22
 perc_agr_22
-# Percentuale zona agricola =  0.5265535
+# Percentuale zona agricola = 68.0842
 
-
-  ## Prova 2 ##
-# Classe 1 = suolo agricolo (bianco)
-# Classe 2 = zona forestale (verde) 
-
-ln2022nC <- unsuperClass(ln2022, nClasses=2)
-ln2022nC
-
-jpeg("ln2022nC")
-plot(ln2022nC$map)
-dev.off()
-
-# Calcolare la frequenza dei pixel che appartengono a ciascuna classe 
-freq(ln2022nC$map)
-
-# Classe 1 = 12807531
-# Classe 2 = 27986377
-# NA 18663113 (non applicabile)
-
-# Calcolare la percentuale di foresta e di area agricola del 2022
-tot2022 <- 59457021
-
-perc_forest_2022 <- 27986377 * 100 / tot2022
-perc_forest_2022
-# Percentuale foresta = 47.06993
-
-perc_agr_2022 <- 12807531 * 100 / tot2022
-perc_agr_2022
-# Percentuale zona agricola =  21.54082
-
-# La classificazione dell'immagine del 2022, porta a due risultati diversi prova 1 e prova 2.
-# Una possibile spiegazione può essere data da un elevato numero di pixel NA, cioè pixel non associati a nessuna delle due classi.
-# La prova 2 è quella che si avvicina di più al risultato atteso, ma comunque poco affidabile.
 
 # Costruire un dataframe con i dati percentuali dell'area forestale e del suolo agricolo
 # Colonne (campi)
 class <- c("Forest", "Agriculture")
-percent_2002 <- c(45.17771, 23.3644)
-percent_2014 <- c(39.66055, 28.58588)
-percent_2022 <- c(47.06993, 21.54082)
+percent_2002 <- c(25409424,12576417)
+percent_2014 <- c(17040113,23641752)
+percent_2022 <- c(314766,40480835)
 
 multitemporal <- data.frame(class, percent_2002, percent_2014, percent_2022)
 multitemporal
